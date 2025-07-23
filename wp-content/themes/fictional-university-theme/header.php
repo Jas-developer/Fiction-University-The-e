e1466cc6edb1d8c04e22746bdee507ed77c7420b
Jason <devop.jas@gmail.com>
2025-07-23 14:36:50 +0800

    Added Meta tag that teals the browser to use it owns size / to be true to their own size

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <!-- head sandwitch -->
 <head>
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(  ); ?>
 </head>
 <!-- body sandwitch -->
