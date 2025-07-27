d3a97f64f465bb84a9fb752ca6397c91d3fbe0c4
Jason <devop.jas@gmail.com>
2025-07-27 15:47:39 +0800

    I added custom data custom post custom desoign dynamuic data

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -15,11 +15,8 @@ add_action('wp_enqueue_scripts', 'university_files');
 function university_features()
 {
  add_theme_support( 'title-tag' );
- //registering a menu
- register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
- register_nav_menu( 'footerMenuLocationOne', 'Footer Location One' );
- register_nav_menu( 'footerMenuLocatonTwo', 'Footer Location Two' );
 }
 
 add_action( 'after_setup_theme', 'university_features' );
+
 
