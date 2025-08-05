9f8b52e6e214e5d13551d52322e58e67be55013b
Jason <devop.jas@gmail.com>
2025-08-06 01:50:49 +0800

    Added

--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -3,20 +3,46 @@ import $ from 'jquery';
 class Search {
     // describe and create our object
     constructor(){
+       this.resultsDiv = $("#search-overlay__results");
        this.openButton = $('.js-search-trigger');
        this.closeButton = $('.search-overlay__close');
        this.searchOverlay = $('.search-overlay') ;
+       this.searchField = $('#search-term');
        this.events();
        this.isOverlayOpen =  false;
+       this.typingTimer;
+       this.previousValue;
     }
 
     // events
     events() {
         this.openButton.on('click',  this.openOverlay.bind(this));
         this.closeButton.on('click',this.closeOverlay.bind(this));
-        $(document).on('keyup', this.keyPressDispatcher.bind(this));
+        $(document).on('keydown', this.keyPressDispatcher.bind(this));
+        this.searchField.on("keyup",this.typingLogic.bind(this));
     }
-    // methods
+
+    typingLogic(){
+
+        clearTimeout(this.typingTimer);
+        if(this.resultsDiv.find('.spinner-loader').length === 0 
+           && this.previousValue != this.searchField.val() ){
+            
+            this.resultsDiv.html('<div class="spinner-loader"></div>');
+
+             this.typingTimer = setTimeout(() =>
+            {
+              this.resultsDiv.html('This is a sample search results');
+            }
+        ,2000);
+        }
+        
+       
+
+        this.previousValue = this.searchField.val();
+    }
+
+
     openOverlay(){
         this.searchOverlay.addClass('search-overlay--active');
         $('body').addClass('body-no-scroll');
