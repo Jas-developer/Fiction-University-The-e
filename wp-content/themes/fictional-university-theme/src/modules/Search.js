9e9d8f2f9001a06c13b84b60245aeecbccda2045
Jason <devop.jas@gmail.com>
2025-08-07 05:30:52 +0800

    worked with asyncrounous working

--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -52,26 +52,36 @@ class Search {
   }
 
   getResults() {
-
-
-    $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' 
-      + this.searchField.val(), posts => {
       
-       this.resultsDiv.html(`
+      const getPostsData = $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val());
+      const getPagesData = $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val());
+
+
+      $.when(getPostsData,getPagesData).then(([posts], [pages]) => {
+      
+      const combineResults = posts.concat(pages);
+      
+      this.resultsDiv.html(`
          <h2 class="search-overlay__section-title">General Information</h2>
-         ${posts.length ? '<ul class="link-list min-list">' : ' <p>No Results found</p>'}
-          ${posts.map(item => `
-            <li >
-             <a href="${item.link}">${item.title.rendered}</a>
-            </li>`
-          ).join(' ')}
-        ${posts.length ? ' </ul >' : ' '}
-        `);
-        
-        this.isSpinnerVisible = false;
+         ${
+           combineResults.length
+             ? `
+             <ul class="link-list min-list">
+               ${combineResults.map(item => `
+                 <li>
+                   <a href="${item.link}">${item.title.rendered}</a>
+               </li>
+               `).join('')}
+              </ul>
+             `
+            : '<p>No Results found</p>'
+         }
+`);
 
-    });
+this.isSpinnerVisible = false;
 
+});
+    
   }
 
   keyPressDispatcher(e) {
