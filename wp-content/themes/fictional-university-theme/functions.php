ac48b26484cc781645693e81ad2aab20ab30462a
Jason <devop.jas@gmail.com>
2025-08-09 05:48:11 +0800

    added new custom field

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,4 +1,18 @@
 <?php 
+
+
+
+
+//added new custom field
+
+function university_custom_rest(){
+  register_rest_field( 'post', 'authorName', array(
+    'get_callback' => function() { return  get_the_author( ); }
+  ) );
+}
+
+add_action('rest_api_init', 'university_custom_rest');
+
 // PAGE BANNNER
 function pageBanner($args = NULL) {
          
