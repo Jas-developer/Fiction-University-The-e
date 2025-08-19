4521cb812addd687293c434242f8b369c35e99c1
Jason <devop.jas@gmail.com>
2025-08-19 17:49:20 +0800

    dopne

--- wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -21,23 +21,106 @@ class MyNotes {
             this.EditNote(e);
             });
         });
+        //Update a Note Event Listener
+        const updateTheNote = document.querySelectorAll('.update-note');
+        updateTheNote.forEach((updateNote) => {
+           updateNote.addEventListener('click', (e) => {
+            this.UpdateNote(e)
+           })
+        })
+
 
 
 
     }
+    
+
+    //Update Note Method Passed as a CallBack Function for the Event Listener
+    async UpdateNote(e){
+
+        try {
+            
+        const parentCard = e.target.closest(".note-card");
+        const noteId = parentCard.getAttribute('data-id');
+       
+
+        // our updated value
+        const titleValue = parentCard.querySelector('.note-title-field').value;
+        const bodyValue = parentCard.querySelector('.note-body-field').value;
+
+        const updatedValue = {
+            title: titleValue,
+            content: bodyValue
+        }
+        
+        const postUrl =  universityData.archive_routes.root_url+`/wp-json/wp/v2/note/${noteId}`;
+        
+        // Sending updated data / Updating data
+        const res = await axios.post(postUrl, updatedValue,
+            {
+                 headers:{
+                'X-WP-Nonce': universityData.archive_routes.nonce
+                 }
+            }
+        )
+        
+
+        console.log(res.data)
+
+
+        // close the button after clicking save
+        const SaveUpdate = () => {
+            // Input Element
+            const inputElement = parentCard.querySelector('.note-title-field');
+            inputElement.classList.remove('note-active-field');
+            inputElement.setAttribute('readonly', true);
+            // TextArea Element
+            const TextAreaElement = parentCard.querySelector(".note-body-field");
+            TextAreaElement.classList.remove('note-active-field');
+            TextAreaElement.setAttribute('readonly', true)
+            // Save Button
+            parentCard.querySelector('.update-note').classList.remove('update-note--visible');
+
+            /*
+            *Edit & Cancel Button
+            *Replaced EDIT Button with CANCEL button when its clicked
+            */
+            const spanElement = parentCard.querySelector('.edit-note');
+            spanElement.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i> Edit';
+            spanElement.setAttribute('data-state', 'noteditable'); 
+        }
+        
+        SaveUpdate();
+            
+        } catch (error) {
+          console.log(error.message ? error.message : "Something went Wrong") 
+        }
+    }
+
+
+
+
+
+
 
     // Edit Note Method Passed as a CallBack Function for the Event Listener
-    async EditNote(e){
-       try {
-        const parentCard = e.target.closest('.note-card');
-        const noteId = parentCard.getAttribute('data-id');
+    async EditNote(e) {
 
+        const parentCard = e.target.closest('.note-card');
+       
+        
+        if(parentCard.querySelector('.edit-note').dataset.state != 'editable'){
+          parentCard.querySelector('.edit-note').setAttribute('data-state', 'editable');   
+        }else{
+          parentCard.querySelector('.edit-note').setAttribute('data-state', 'noteditable');  
+        }
+      
         /*
          *Access Save Button Element, Remove Ready only Attr for Input & Textarea And Add  Active class for styling when .edit-note element clicked
          *Replace Edit Button by Cancel Button after Clicked
         */
-        
-        const editNoteTitleAndBody = () => {
+
+        const editingNoteMode = () => {
             // Input Element
             const inputElement = parentCard.querySelector('.note-title-field');
             inputElement.classList.add('note-active-field');
@@ -55,26 +138,39 @@ class MyNotes {
             */
             const spanElement = parentCard.querySelector('.edit-note');
             spanElement.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i> Cancel';
+            
+            
         }
-        // called the function
-        editNoteTitleAndBody();
+        
        
         
-        
+        const cancelNoteEditingMode = () => {
+            // Input Element
+            const inputElement = parentCard.querySelector('.note-title-field');
+            inputElement.classList.remove('note-active-field');
+            inputElement.setAttribute('readonly', true);
+            // TextArea Element
+            const TextAreaElement = parentCard.querySelector(".note-body-field");
+            TextAreaElement.classList.remove('note-active-field');
+            TextAreaElement.setAttribute('readonly', true)
+            // Save Button
+            parentCard.querySelector('.update-note').classList.remove('update-note--visible');
 
-    
+            /*
+            *Edit & Cancel Button
+            *Replaced EDIT Button with CANCEL button when its clicked
+            */
+            const spanElement = parentCard.querySelector('.edit-note');
+            spanElement.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i> Edit';
+        }
 
-        const postUrl =  universityData.archive_routes.root_url+`/wp-json/wp/v2/note/${noteId}`;
-         
-        
+        // Display note depending on state attribute value
+        if(parentCard.querySelector('.edit-note').dataset.state == 'editable'){
+           editingNoteMode()
+          }else{
+           cancelNoteEditingMode();
+        }
 
-
-
-
-       } catch (error) {
-         const errorMessage = error.message ? error.message : "Something Went Wrong";
-         console.log(errorMessage);
-       } 
     }
    
 
